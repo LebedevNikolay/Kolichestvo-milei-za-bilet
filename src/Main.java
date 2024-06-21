40ea1a9ce2397f2b9b450a65fa012783ffa6a0dd
LebedevNikolay <nikolay-kmc@mail.ru>
2024-06-21 14:15:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        int ticketPrice = 11404; //стоимость билета
+        int bonus = 20; //сумма одной бонусной мили
 
+        int miles;  //Переменная мили.
+        if (ticketPrice < 0) {
+            System.out.printf("Стоимость билета не может быть отрицательной"); //проверка отрицательной стоимости билета
+        } else if (ticketPrice > 0) {
+            miles = ticketPrice / bonus;//расчет количества миль, за покупку билета.
+            System.out.printf("Кол-во миль за покупку билета: " + miles);// Вывод результата.
 
-        int ticketPrice = 128718; // Переменная стоимости билета
-
-        int miles = Math.round(ticketPrice / 20); // Переменная бонусных миль
-
-        System.out.println("Кол-во баллов за купленый билет:");
-        System.out.println(miles);
     }
-
+    }
 }
